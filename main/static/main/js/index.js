1a8b4fe01a4b9107f1807175aea34b5833fa9567
Wan <Joachim.Wan@shell.com>
2022-06-28 16:35:49 +0800

    swapped index with index_test

--- main/static/main/js/index.js
+++ main/static/main/js/index.js
@@ -1,81 +1,55 @@
 function updateProject(id) {
-//    var element = document.getElementById("project-"+id);
-//    if (elem.style.display === "none") {
-//        default_block.style.display = "block";
-//    } else {
-//        default_block.style.display = "none";
-//    }
+    let hiddenable_element_ids = [
+        "div-project-name-",
+        "form-update-project-",
+        "undo-button-toggle-update-project-",
+//        "button-delete-project-"
+    ];
+    hiddenable_element_ids.forEach(function(element_name){
+        let element = document.getElementById(element_name+id);
+        let hidden_attr = element.getAttribute("hidden");
+        if (hidden_attr) {
+            element.removeAttribute("hidden");
+        } else {
+            element.setAttribute("hidden", true);
+        }
+    });
 
-    var element = document.getElementById("div-project-name-"+id);
-    let hidden1 = element.getAttribute("hidden");
-    if (hidden1) {
-       element.removeAttribute("hidden");
-    } else {
-       element.setAttribute("hidden", true);
-    }
-
-    var b = document.getElementById("form-update-project-"+id);
-    let hidden2 = b.getAttribute("hidden");
-    if (hidden2) {
-       b.removeAttribute("hidden");
-    } else {
-       b.setAttribute("hidden", true);
-    }
-
-    var c = document.getElementById("button-toggle-update-project-"+id);
-    let hidden3 = c.getAttribute("hidden");
-    if (hidden3) {
-       c.removeAttribute("hidden");
-    } else {
-       c.setAttribute("hidden", true);
-    }
-
-    var d = document.getElementById("button-delete-project-"+id);
-    let hidden4 = d.getAttribute("hidden");
-    if (hidden4) {
-       d.removeAttribute("hidden");
-    } else {
-       d.setAttribute("hidden", true);
-    }
+    let other_buttons = document.querySelectorAll('[id^="button"]');
+    other_buttons.forEach(function(element){
+        let hidden_attr = element.getAttribute("hidden");
+        if (hidden_attr) {
+            element.removeAttribute("hidden");
+        } else {
+            element.setAttribute("hidden", true);
+        }
+    });
 }
 
 function updateWell(id) {
-//    var default_block = document.getElementById("well-"+id);
-//    if (default_block.style.display === "none") {
-//        default_block.style.display = "block";
-//    } else {
-//        default_block.style.display = "none";
-//    }
+    let hiddenable_element_ids = [
+        "div-well-name-",
+        "form-update-well-",
+        "undo-button-toggle-update-well-",
+//        "button-delete-well-"
+    ];
+    hiddenable_element_ids.forEach(function(element_name){
+        let element = document.getElementById(element_name+id);
+        let hidden_attr = element.getAttribute("hidden");
+        if (hidden_attr) {
+            element.removeAttribute("hidden");
+        } else {
+            element.setAttribute("hidden", true);
+        }
+    });
 
-    var element = document.getElementById("div-well-name-"+id);
-    let hidden1 = element.getAttribute("hidden");
-    if (hidden1) {
-       element.removeAttribute("hidden");
-    } else {
-       element.setAttribute("hidden", true);
-    }
-
-    var b = document.getElementById("form-update-well-"+id);
-    let hidden2 = b.getAttribute("hidden");
-    if (hidden2) {
-       b.removeAttribute("hidden");
-    } else {
-       b.setAttribute("hidden", true);
-    }
-
-    var c = document.getElementById("button-toggle-update-well-"+id);
-    let hidden3 = c.getAttribute("hidden");
-    if (hidden3) {
-       c.removeAttribute("hidden");
-    } else {
-       c.setAttribute("hidden", true);
-    }
-
-    var d = document.getElementById("button-delete-well-"+id);
-    let hidden4 = d.getAttribute("hidden");
-    if (hidden4) {
-       d.removeAttribute("hidden");
-    } else {
-       d.setAttribute("hidden", true);
-    }
+    let other_buttons = document.querySelectorAll('[id^="button"]');
+    other_buttons.forEach(function(element){
+        let hidden_attr = element.getAttribute("hidden");
+        if (hidden_attr) {
+            element.removeAttribute("hidden");
+        } else {
+            element.setAttribute("hidden", true);
+        }
+    });
 }
